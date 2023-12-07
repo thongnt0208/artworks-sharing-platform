d1de90f04f56bb498f6b1ade5e2624e08cfbe3e0
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-12-07 19:39:53 +0700

    Anh - Create common components, router and fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomeScreen from "./layout/HomeScreen/HomeScreen";
 
 import { PrimeReactProvider } from "primereact/api";
@@ -6,11 +8,22 @@ import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <PrimeReactProvider>
-      <HomeScreen />
+      <BrowserRouter>
+
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>      
+      
+      <Footer />
+      
+      </BrowserRouter>
     </PrimeReactProvider>
   );
 }
