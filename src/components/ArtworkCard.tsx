d47f6b40006c2827936cbe741d81d1c74beaf5a0
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-12-08 10:12:23 +0700

    Thong - ArtworkCard component

--- src/components/ArtworkCard.tsx
+++ src/components/ArtworkCard.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import "./style.scss";
 
-type Props = {}
+type Props = {
+  id: string;
+  title: string;
+  subTitle: string;
+  image: string;
+  likeNum: number;
+  viewNum: number;
+  likeHandler?: () => void;
+  viewHandler?: () => void;
+  saveHandler?: () => void;
+};
 
-function ArtworkCard({}: Props) {
+const ArtworkCard: React.FC<Props> = ({ ...props }: Props) => {
+  let header = (
+    <div className="header-container">
+      <div className="thumbnail-container">
+        <img
+          alt={`Hình thu nhỏ của tác phẩm tên ${props.title}`}
+          src={props.image}
+          className="thumbnail border-round"
+        />
+      </div>
+
+      <Button
+        label={props.viewNum.toString()}
+        icon="pi pi-eye"
+        onClick={props.viewHandler}
+        className="view-button"
+      />
+    </div>
+  );
+  let footer = (
+    <>
+      <Button
+        label={props.likeNum.toString()}
+        icon="pi pi-heart"
+        onClick={props.likeHandler}
+      />
+
+      <Button
+        label="Save"
+        icon="pi pi-download"
+        onClick={props.saveHandler}
+        style={{ marginLeft: "0.5em" }}
+      />
+    </>
+  );
   return (
-    <div>ArtworkCard</div>
-  )
-}
+    <Card
+      id={props.id}
+      title={props.title}
+      subTitle={props.subTitle}
+      header={header}
+      footer={footer}
+      className="artwork-card cursor-pointer"
+      onClick={props.viewHandler}
+    ></Card>
+  );
+};
 
-export default ArtworkCard
+export default ArtworkCard;
