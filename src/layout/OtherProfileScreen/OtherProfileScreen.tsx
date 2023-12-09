68803a413b98bf19c0c84167007c4993a5a68e64
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-12-09 11:11:53 +0700

    Anh - Create Other Profile Screen

---
+++ src/layout/OtherProfileScreen/OtherProfileScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OtherProfileScreen: React.FC = () => {
+  return (
+    <div>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default OtherProfileScreen;
