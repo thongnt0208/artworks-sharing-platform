662e7ad91b4150e3d54b25ff9248e2f48e30475d
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-12-12 02:07:02 +0700

    Refactor login functionality and add LoginScreen component

--- src/layout/LoginScreen/LoginScreen.tsx
+++ src/layout/LoginScreen/LoginScreen.tsx
@@ -1,12 +1,93 @@
-import React, { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import AuthContext from "../../auth/AuthContext";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import { login } from "../../auth/AuthService";
+import { useNavigate } from "react-router-dom";
 
-const LoginScreen: React.FC = () => {
-  const { loginHandler } = useContext(AuthContext);
+const LoginScreen = () => {
+  const { setAuthenticationInfo } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ username: "", password: "" });
 
-  const handleForm = () => {
-    // Handle form submission, call login function from context
-    // loginHandler();
+  const navigate = useNavigate();
+  const toast: any = useRef(null);
+
+  const validateInputs = () => {
+    let valid = true;
+    const newErrors = { username: "", password: "" };
+
+    if (!username) {
+      newErrors.username = "Username is required";
+      valid = false;
+    }
+
+    if (!password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
   };
-  return <></>;
+
+  const handleLogin = () => {
+    if (validateInputs()) {
+      setTimeout(() => {
+        login(username, password)
+          .then((response) => {
+            const accessToken = response?.data?.accessToken;
+            const role = response?.data?.role;
+            setAuthenticationInfo({ username, password, accessToken, role });
+            navigate("/");
+            toast.current.show({
+              severity: "success",
+              summary: "Login Successful",
+              detail: "You have been logged in successfully",
+              life: 3000,
+            });
+          })
+          .catch((error) => {
+            if (error?.status !== 200) {
+              toast.current.show({
+                severity: "error",
+                summary: "Login Failed",
+                detail: `There was an error logging in, please try again later.`,
+                life: 3000,
+              });
+            }
+          });
+      });
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <Toast ref={toast} />
+      <div className="p-field">
+        <label htmlFor="username">Username</label>
+        <InputText
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <small className="p-error">{errors.username}</small>
+      </div>
+      <div className="p-field">
+        <label htmlFor="password">Password</label>
+        <InputText
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <small className="p-error">{errors.password}</small>
+      </div>
+      <Button label="Login" onClick={handleLogin} />
+    </div>
+  );
 };
+
+export default LoginScreen;
