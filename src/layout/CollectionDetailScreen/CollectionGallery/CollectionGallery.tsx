6040349949314f47b1a007608fadf4dfdd63d7a1
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-12-12 16:20:30 +0700

    Anh - Create Collection Detail Screen

---
+++ src/layout/CollectionDetailScreen/CollectionGallery/CollectionGallery.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ArtworkCard from "../../../components/ArtworkCard";
+import "./CollectionGallery.scss";
+
+interface ArtworksProps {
+  id: string;
+  title: string;
+  subTitle: string;
+  imageUrl: string;
+  likeNum: number;
+  viewNum: number;
+}
+
+const CollectionGallery: React.FC<{ artworks: ArtworksProps[] }> = ({
+  artworks,
+}) => {
+  return (
+    <div className="collection-gallery">
+      <h1>Collection 1</h1>
+      <div className="gallery">
+        {artworks.map((artwork) => (
+          <ArtworkCard
+            key={artwork.id}
+            id={artwork.id}
+            title={artwork.title}
+            subTitle={artwork.subTitle}
+            image={artwork.imageUrl}
+            likeNum={10}
+            viewNum={12}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionGallery;
