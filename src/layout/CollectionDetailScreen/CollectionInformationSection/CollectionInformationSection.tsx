b2b8b5e2be5da7dd2f1f2dd240d9a17a54f217a6
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-12-12 21:23:13 +0700

    Anh - Update data of Collection Information

--- src/layout/CollectionDetailScreen/CollectionInformationSection/CollectionInformationSection.tsx
+++ src/layout/CollectionDetailScreen/CollectionInformationSection/CollectionInformationSection.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Image } from 'primereact/image';
+import React from "react";
+import { Image } from "primereact/image";
 
-const logo = require('../../../assets/logo/logo-small.png');
+const logo = require("../../../assets/logo/logo-small.png");
 
-const CollectionInformationSection: React.FC= () => {
+type CollectionInformationProps = {
+  collectionInfo: {
+    id: string;
+    createdBy: string;
+    collectionName: string;
+    privacy: string;
+    creationDate: string;
+  };
+  // artworkCounter: number;
+};
+
+const CollectionInformationSection: React.FC<CollectionInformationProps> = ({
+  collectionInfo,
+}: CollectionInformationProps) => {
+  const { id, createdBy, collectionName, privacy, creationDate } =
+    collectionInfo;
+
   return (
-    <div>   
-        <Image src={logo} alt="Logo" />
-        <h1>Collection 1</h1>
-        <p>Mot bo suu tap boi <strong>Nguyen Trung Thong</strong> </p>
-        <p>1 tac pham</p>
-        <button>Chinh sua</button>
+    <div>
+      <Image src={logo} alt="Logo" />
+      <h1>{collectionName}</h1>
+      <p>
+        Mot bo suu tap boi <strong>{createdBy}</strong>{" "}
+      </p>
+      {/* <p>{artworkCounter} tac pham</p> */}
+      <button>Chinh sua</button>
     </div>
   );
 };
+
 
 export default CollectionInformationSection;
