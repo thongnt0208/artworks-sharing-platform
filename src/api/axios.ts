31d0c6fb5cd1933e55b232e271d3807f9bf7aea4
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-12-13 14:55:17 +0700

    Update axios configuration

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+const BASE_URL = "https://dummyjson.com";
+
 export default axios.create({
-  baseURL: process.env.NODE_ENV_BASE_URL || "https://dummyjson.com",
+  baseURL: process.env.NODE_ENV_BASE_URL || BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: process.env.NODE_ENV_BASE_URL || BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
