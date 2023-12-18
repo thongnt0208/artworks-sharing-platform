29baae3842f2ab23efa23f713f90c0fec75950af
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-12-18 23:19:34 +0700

    Add routes and components for artwork management

--- src/layout/PostArtworkScreen/PostArtworkScreen.tsx
+++ src/layout/PostArtworkScreen/PostArtworkScreen.tsx
@@ -1,9 +1,39 @@
-import "./PostArtworkScreen.scss"
+import { useNavigate, useParams } from "react-router-dom";
+import "./PostArtworkScreen.scss";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import Content from "./Content";
+{
+  /* Import Content.ts in /ArtworkDetailScreen/content/ */
+}
 
-type Props = {}
+type Props = {};
 
-export default function PostArtworkScreen({}: Props) {
+export default function PostArtworkScreen({ ...props }: Props) {
+  const navigate = useNavigate();
+  const [data, setData] = useState({} as any);
+  const [error, setError] = useState({} as any);
+  const { authenticationInfo } = useAuth();
+
+  useEffect(() => {}, []);
+
   return (
-    <div>PostArtworkScreen</div>
-  )
+    <>
+      {/* <p>{JSON.stringify(data)}</p> */}
+
+      {data.Images && (
+        <div className="artwork-detail-container">
+          <div className="detail-container flex grid-nogutter">
+            <div className="left-panel-container col col-10">
+              {/* Import Content.ts in /ArtworkDetailScreen/content/ */}
+            </div>
+            <div className="right-panel-container col col-2">
+              {/* Form start here */}
+              {/* Form end here */}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
