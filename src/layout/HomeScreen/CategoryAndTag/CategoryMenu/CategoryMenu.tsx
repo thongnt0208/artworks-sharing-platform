dd004bde65744832fd8f3310090acb57fd6966c8
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2023-12-19 09:14:20 +0700

    Anh - Fix file structure

--- src/layout/HomeScreen/CategoryAndTag/CategoryMenu/CategoryMenu.tsx
+++ src/layout/HomeScreen/CategoryAndTag/CategoryMenu/CategoryMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Tag from "../../../../components/Tag";
 import "./CategoryMenu.scss"
+import { GenerateRandomColorCode } from "../../../../util/GenerateRandomColorCode";
 
 type CategoryProps = {
   id: string;
@@ -11,34 +12,11 @@ type CategoriesProps = {
   categories: CategoryProps[];
 };
 
-// Generate a random color code
-const getRandomColorCode = (): string => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-// Generate an array of random color codes
-const getRandomColorCodes = (count: number): string[] => {
-  const colorCodes: string[] = [];
-  for (let i = 0; i < count; i++) {
-    colorCodes.push(getRandomColorCode());
-  }
-  return colorCodes;
-};
-
-// Example usage
-const randomColorCodes = getRandomColorCodes(3);
-console.log(randomColorCodes); // Output: e.g. ["#1A2B3C", "#4D5E6F", "#7G8H9I", "#JKLMNO", "#PQRSTU"]
-
 const CategoryMenu: React.FC<CategoriesProps> = ({ categories }) => {
   return (
     <div className="category-menu">
         {categories.map((category) => {
-          const randomColorCode = getRandomColorCode();
+          const randomColorCode = GenerateRandomColorCode();
           return <Tag id={category.id} tagName={category.categoryName} color={randomColorCode} />;
         })}
     </div>
--- src/layout/HomeScreen/CategoryAndTag/TagCarousel/TagCarousel.tsx
+++ src/layout/HomeScreen/CategoryAndTag/TagCarousel/TagCarousel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Carousel } from "primereact/carousel";
 import Tag from "../../../../components/Tag";
+import { GenerateRandomColorCode } from "../../../../util/GenerateRandomColorCode";
 
 type TagProps = {
   id: string;
@@ -11,35 +12,10 @@ type TagsProps = {
   tags: TagProps[];
 };
 
-
-// Generate a random color code
-const getRandomColorCode = (): string => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-// Generate an array of random color codes
-const getRandomColorCodes = (count: number): string[] => {
-  const colorCodes: string[] = [];
-  for (let i = 0; i < count; i++) {
-    colorCodes.push(getRandomColorCode());
-  }
-  return colorCodes;
-};
-
-// Example usage
-const randomColorCodes = getRandomColorCodes(3);
-console.log(randomColorCodes); // Output: e.g. ["#1A2B3C", "#4D5E6F", "#7G8H9I", "#JKLMNO", "#PQRSTU"]
-
-
 const TagCarousel: React.FC<TagsProps> = ({ tags }) => {
 
   const renderTag = (tag: TagProps) => {
-    const randomColorCode = getRandomColorCode();
+    const randomColorCode = GenerateRandomColorCode();
     return <Tag key={tag.id} id={tag.id} tagName={tag.tagName} color={randomColorCode}/>;
   };
 
