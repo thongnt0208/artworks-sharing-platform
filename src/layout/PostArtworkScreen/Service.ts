e89b28053cff97851c37dcf5f4184202a4a4bd11
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2023-12-19 16:12:42 +0700

    Update ArtworkDetailType and InputForm to use Category array

---
+++ src/layout/PostArtworkScreen/Service.ts
@@ -0,0 +1,16 @@
+/**
+ * This function to POST an artwork to the database
+ * 
+ * @param
+ * @return
+ * @example
+ * @author ThongNT
+ * @version 0.0.0
+ */
+export function postArtwork() {
+//    Function content
+}
+
+/**
+ * Get Category list
+ */
