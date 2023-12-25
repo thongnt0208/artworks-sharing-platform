ba1e02e13b218c45f3c1ca022162b9212d908709
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2023-12-25 21:54:55 +0700

    Update variables and styles, add media queries, and modify button list and content

--- src/layout/ArtworkDetailScreen/content/Content.tsx
+++ src/layout/ArtworkDetailScreen/content/Content.tsx
@@ -1,28 +1,55 @@
 import { Image } from "primereact/image";
 import { ArtworkDetailType } from "./ArtworkDetailType";
 import { Link } from "react-router-dom";
+import "./Content.scss";
+import { useState } from "react";
+import { Button } from "primereact/button";
 
 export default function Content(
   { ...props }: ArtworkDetailType,
   onTagClick?: (tag: string) => void
 ) {
+  const [showAllImages, setShowAllImages] = useState(false);
+
+  const toggleShowImages = () => {
+    setShowAllImages(!showAllImages);
+  };
+
   return (
     <>
       <div className="title-container">
-        <h1>{props.Title}</h1>
+        <h1 className="text-cus-h1-bold">{props.Title}</h1>
       </div>
-      <div className="artwork-images-container">
+      <div
+        className="artwork-images-container"
+        style={{
+          maxHeight: showAllImages ? "none" : "800px",
+          overflow: "hidden",
+        }}
+      >
         {/* Displaying images */}
         {props.Images.map((image, index) => (
-          <Image key={index} src={image} alt={`Image ${index + 1}`} />
+          <Image
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+            width="100%"
+          />
         ))}
       </div>
+
       <div className="artwork-info-container">
+        {/* Show more button */}
+        <div className="showmore-btn-container">
+          <Button onClick={toggleShowImages} className="show-more-button">
+            {showAllImages ? "Thu gọn" : "Xem thêm"}
+          </Button>
+        </div>
         <p>{props.Description}</p>
         <div className="flex gap-3">
-          {props.Tags.map((tag, index) => (
-            <Link key={index} to={""}>
-              #{tag}
+          {props.Tags.map((tag: any) => (
+            <Link key={tag.Id} to={""}>
+              #{tag.TagName}
             </Link>
           ))}
         </div>
