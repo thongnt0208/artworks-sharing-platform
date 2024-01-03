ac9a603b5b1496406e13929ed2b68a24241af45d
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-01-03 15:00:15 +0700

    Anh - Update UI

--- src/layout/ProfileScreen/ArtworkScreen/ArtworkScreen.tsx
+++ src/layout/ProfileScreen/ArtworkScreen/ArtworkScreen.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router";
+
+import { GetArtworksData } from "./ArtworkService";
 import "./ArtworkScreen.scss";
 import ArtworkCard from "../../../components/ArtworkCard";
-import { GetArtworksData } from "./ArtworkService";
 
 type ArtworksProps = {
   id: string;
@@ -12,12 +16,17 @@ type ArtworksProps = {
   viewNum: number;
 };
 
-const ArtworkManagement: React.FC = () => {
+const ArtworkManagement: React.FC<{ isLogin: boolean }> = ({ isLogin }) => {
+  let navigate = useNavigate();
   const [artworks, setArtworks] = React.useState<ArtworksProps[]>([]);
   React.useEffect(() => {
     const fetchArtworks = async () => {
       const response = await GetArtworksData();
-      setArtworks(response);
+      if (Array.isArray(response)) {
+        setArtworks(response);
+      } else {
+        console.error("Response is not an array:", response);
+      }
     };
     fetchArtworks();
   }, []);
@@ -25,19 +34,35 @@ const ArtworkManagement: React.FC = () => {
     <>
       <h1>Các tác phẩm</h1>
       <div className="gallery grid">
-        {artworks.map((artwork) => (
-          <div className="gallery__item col col-6" key={artwork.id}>
-            <ArtworkCard
-              key={artwork.id}
-              id={artwork.id}
-              title={artwork.title}
-              subTitle={artwork.subTitle}
-              image={artwork.image}
-              likeNum={10}
-              viewNum={12}
-            />
-          </div>
-        ))}
+        {artworks.length === 0 ? (
+          isLogin ? (
+            <Card className="add-artwork-card cursor-pointer flex flex-column justify-content-center align-items-center">
+              <i className="pi pi-plus-circle icon m-3" />
+              <Button
+                label="Tạo tác phẩm"
+                onClick={() => {
+                  navigate("/postAw");
+                }}
+              ></Button>
+            </Card>
+          ) : (
+            <div> Tác giả chưa có tác phẩm nào </div>
+          )
+        ) : (
+          artworks.map((artwork) => (
+            <div className="gallery__item col col-6" key={artwork.id}>
+              <ArtworkCard
+                key={artwork.id}
+                id={artwork.id}
+                title={artwork.title}
+                subTitle={artwork.subTitle}
+                image={artwork.image}
+                likeNum={10}
+                viewNum={12}
+              />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
