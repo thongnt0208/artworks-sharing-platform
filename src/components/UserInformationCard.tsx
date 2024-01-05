e464950802ac623de3d10b133041eda5b7591f9f
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-01-05 12:40:47 +0700

    Anh - Update Profile Screen UI

--- src/components/UserInformationCard.tsx
+++ src/components/UserInformationCard.tsx
@@ -1,70 +1,49 @@
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
-import "./UserInformationCard.scss";
 import { useState } from "react";
+import "./UserInformationCard.scss";
 
-/* Sample props:
-
-const userProps = {
-    id: "123",
-    name: "Thong Nguyen",
-    isCreator: true,
-    job: "Developer",
-    address: "Lost Angelest",
-    introduction: "I am Front-end Developer",
-    profileView: 99,
-    artworksView: 999,
-    followerNum: 9,
-    followingNum: 99,
-    followHandler: () => {},
-    hireHandler: () => {},
-    editHandler: () => {},
-    privacyEditHandler: () => {},
-  };
-*/
+const defaultAvatar = require("../assets/defaultImage/default-avatar.png");
 
 type Props = {
   id: string;
-  name: string;
-  isCreator: boolean; // true if it is current user, false if it is audience
-  job: string;
-  address: string;
-  introduction: string;
+  username: string;
+  fullname: string;
+  role: string;
+  // job: string;
+  // address: string;
+  bio: string;
+  avatar: string;
   profileView: number;
   artworksView: number;
   followerNum: number;
   followingNum: number;
+  isCreator: boolean;
   followHandler?: () => void;
   hireHandler?: () => void;
   editHandler?: () => void;
   privacyEditHandler?: () => void;
 };
 
-export default function UserInformationCard({ ...props }: Props) {
-  //   let header = <></>;
-  const [showMore, setShowMore] = useState(false);
-
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
-
+const UserInformationCard: React.FC<Props> = (props: Props) => {
   let footer = (
     <>
-      {props.isCreator && (
+      {props.isCreator ? (
         <>
-          <Button label="Chỉnh sửa trang cá nhân" onClick={props.editHandler} />
+          <Button rounded className="top-button" label="Chỉnh sửa trang cá nhân" onClick={props.editHandler} />
           <Button
+          rounded
+            className="bot-button" 
             label="Thay đổi chế độ riêng tư"
             onClick={props.privacyEditHandler}
           />
         </>
-      )}
-      {!props.isCreator && (
+      ) : (
         <>
-          <Button label="Theo dõi" onClick={props.followHandler} />
-          <Button label="Thuê" onClick={props.hireHandler} />
+          <Button rounded className="top-button" label="Theo dõi" onClick={props.followHandler} />
+          <Button rounded className="bot-button" label="Thuê" onClick={props.hireHandler} />
         </>
-      )}{" "}
+      )}
     </>
   );
   return (
@@ -72,58 +51,58 @@ export default function UserInformationCard({ ...props }: Props) {
       <Card footer={footer} className="user-information-card">
         <div className="avatar-container">
           <img
-            alt={`Ảnh đại diện của ${props.name}`}
-            src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
-            className="thumbnail border-round"
+            alt={`Ảnh đại diện của ${props.fullname}`}
+            src={props.avatar ? props.avatar : defaultAvatar}
+            className="avatar-image"
           />
         </div>
         <div className="information-container">
-          <h1>{props.name}</h1>
-          <p>{props.job}</p>
-          <p>{props.address}</p>
-          {showMore && <p>{props.introduction}</p>}
+          <h1>{props.fullname}</h1>
+          {/* <p>{props.job}</p>
+          <p>{props.address}</p> */}
+          <p>Nghề nghiệp</p>
+          <p>Quận Gò Vấp, Hồ Chí Minh</p>
         </div>
-        <Button
-          label={showMore ? "Thu gọn" : "Xem thêm"}
-          onClick={toggleShowMore}
-        />
-        {showMore && (
-          <div className="views-container grid-nogutter">
-          {/* Column for labels */}
-          <div className="col-9">
-              <div>
-                  <p>Lượt xem trang cá nhân</p>
-              </div>
-              <div>
-                  <p>Lượt xem tác phẩm</p>
-              </div>
-              <div>
-                  <p>Người theo dõi</p>
-              </div>
-              <div>
-                  <p>Đang theo dõi</p>
-              </div>
-          </div>
-      
-          {/* Column for numbers */}
-          <div className="col-3">
-              <div>
-                  <p>{props.profileView}</p>
-              </div>
-              <div>
-                  <p>{props.artworksView}</p>
-              </div>
-              <div>
-                  <p>{props.followerNum}</p>
-              </div>
-              <div>
-                  <p>{props.followingNum}</p>
-              </div>
-          </div>
-      </div>
-      
-        )}
       </Card>
+      <div className="views-container grid-nogutter w-full">
+        {/* Column for labels */}
+        <div className="col-9 pl-4 text-left">
+          <div>
+            <p>Lượt xem trang cá nhân</p>
+          </div>
+          <div>
+            <p>Lượt xem tác phẩm</p>
+          </div>
+          <div>
+            <p>Người theo dõi</p>
+          </div>
+          <div>
+            <p>Đang theo dõi</p>
+          </div>
+        </div>
+
+        {/* Column for numbers */}
+        <div className="col-3 text-left">
+          <div>
+            <p>{props.profileView}</p>
+          </div>
+          <div>
+            <p>{props.artworksView}</p>
+          </div>
+          <div>
+            <p>{props.followerNum}</p>
+          </div>
+          <div>
+            <p>{props.followingNum}</p>
+          </div>
+        </div>
+      </div>
+      <div className="bio-container w-full pl-4 pr-4">
+        <h3>Về tôi</h3>
+        <p className="text-justify">{props.bio}</p>
+      </div>
     </>
   );
-}
+};
+
+export default UserInformationCard;
