92aadb0307f79255755b0d98daa0e01c5d18223c
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-01-08 00:37:16 +0700

    Anh - Update Service Tab UI Profile Screen

---
+++ src/layout/ProfileScreen/ServicesScreen/ServicesScreen.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import ServiceCard from "../../../components/ServiceCard";
+import { GetServicesData } from "./ServicesService";
+import { useOutletContext } from "react-router-dom";
+
+import "./ServicesScreen.scss";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+
+type ServiceCardProps = {
+  id: string;
+  serviceName: string;
+  description: string;
+  deliveryTime: number;
+  numberOfConcept: number;
+  numberOfRevision: number;
+  startingPrice: number;
+  coverLocation: string;
+};
+
+const ServicesScreen: React.FC = () => {
+  const [services, setServices] = useState<ServiceCardProps[]>([]);
+  // let [accountId, isCreator] = useOutletContext() as [string, boolean];
+  let isCreator = true;
+  useEffect(() => {
+    const fetchServices = async () => {
+      const response = await GetServicesData();
+      if (Array.isArray(response)) {
+        setServices(response);
+      } else {
+        console.error("Response is not an array:", response);
+      }
+    };
+    fetchServices();
+  }, []);
+
+  return (
+    <>
+      <h1>Các dịch vụ</h1>
+      <div className="gallery p-0">
+        {services.length === 0 ? (
+          isCreator ? (
+            <Card className="add-service-card cursor-pointer flex flex-column justify-content-center align-items-center">
+              <i className="pi pi-plus-circle icon m-3" />
+              <Button label="Tạo dịch vụ" onClick={() => {}}></Button>
+            </Card>
+          ) : (
+            <div> Tác giả chưa có tác phẩm nào </div>
+          )
+        ) : (
+          services.map((service) => (
+            <div className="gallery__item col col-6" key={service.id}>
+              <ServiceCard
+                key={service.id}
+                id={service.id}
+                serviceName={service.serviceName}
+                description={service.description}
+                deliveryTime={service.deliveryTime}
+                numberOfConcept={service.numberOfConcept}
+                numberOfRevision={service.numberOfRevision}
+                startingPrice={service.startingPrice}
+                coverLocation={service.coverLocation}
+              />
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ServicesScreen;
