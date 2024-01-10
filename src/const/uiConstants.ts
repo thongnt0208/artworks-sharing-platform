19375c89c5e5c936e01a95539666cdfe0dba52ed
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-01-10 17:10:14 +0700

    Add ChatScreen component and Init header visibility feature

---
+++ src/const/uiConstants.ts
@@ -0,0 +1,16 @@
+export const hideFooterRoutes = [
+  "login",
+  "register",
+  "forgot-password",
+  "reset-password",
+  "change-password",
+  "chat",
+];
+
+export const hideHeaderRoutes = [
+  "login",
+  "register",
+  "forgot-password",
+  "reset-password",
+  "change-password",
+];
