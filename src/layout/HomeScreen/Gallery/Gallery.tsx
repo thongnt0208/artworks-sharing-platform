f7856e7eb23cab111cea9b453ab0e62528c8a87c
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-01-11 13:48:59 +0700

    Anh - Change Artwork type for integration and update node-red

--- src/layout/HomeScreen/Gallery/Gallery.tsx
+++ src/layout/HomeScreen/Gallery/Gallery.tsx
@@ -6,9 +6,9 @@ import { useNavigate } from "react-router-dom";
 type Artwork = {
   id: string;
   title: string;
-  creatorId: string;
-  creator: string;
-  image: string;
+  createdBy: string;
+  creatorFullName: string;
+  thumbnail: string;
   likeNum: number;
   viewNum: number;
 };
@@ -19,7 +19,7 @@ type ArtworksProps = {
 
 const Gallery: React.FC<ArtworksProps> = ({ artworks }) => {
   const navigate = useNavigate();
-
+  console.log(artworks);
   return (
     <div className="gallery grid p-0 flex justify-content-start">
       {artworks
@@ -29,9 +29,9 @@ const Gallery: React.FC<ArtworksProps> = ({ artworks }) => {
               key={artwork.id}
               id={artwork.id}
               title={artwork.title}
-              creatorId={artwork.creatorId}
-              creator={artwork.creator}
-              image={artwork.image}
+              createdBy={artwork.createdBy}
+              creatorFullName={artwork.creatorFullName}
+              thumbnail={artwork.thumbnail}
               likeNum={artwork.likeNum}
               viewNum={artwork.viewNum}
               viewHandler={() => navigate(`/artwork/${artwork.id}`)}
