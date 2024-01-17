d96a6e9f3f1838a6a4ae7d1cb691604424a0f797
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-01-17 09:15:09 +0700

    Anh - Refactor assets view and menu tab components

--- src/layout/ProfileScreen/AssetsView/AssetsView.tsx
+++ src/layout/ProfileScreen/AssetsView/AssetsView.tsx
@@ -3,6 +3,7 @@ import { GetAssetsData } from "./AssetsService";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import { useNavigate } from "react-router";
+import { useOutletContext } from "react-router-dom";
 
 import AssetsCard from "../../../components/AssetsCard";
 import "./AssetsView.scss";
@@ -27,54 +28,63 @@ type AssetsProps = {
   onClickHandler?: () => void;
 };
 
-const AssetsView: React.FC<{ isLogin: boolean }> = ({ isLogin }) => {
+const AssetsView: React.FC = () => {
   let navigate = useNavigate();
+  let [accountId, isCreator] = useOutletContext() as [string, boolean];
   const [assets, setAssets] = React.useState<AssetsProps[]>([]);
+
   React.useEffect(() => {
-    const fetchArtworks = async () => {
-      const response = await GetAssetsData();
+    const fetchAssets = async () => {
+      const response = await GetAssetsData(accountId);
+      console.log("Response:", response);
       if (Array.isArray(response)) {
         setAssets(response);
       } else {
         console.error("Response is not an array:", response);
       }
     };
-    fetchArtworks();
+    fetchAssets();
   }, []);
+
   return (
-    <>
+    <div className="assets-gallery-container">
       <h1>Các tài nguyên</h1>
       <div className="gallery">
         {assets.length === 0 ? (
-          isLogin ? (
-            <Card className="add-asset-card cursor-pointer flex flex-column justify-content-center align-items-center">
-              <i className="pi pi-plus-circle icon m-3" />
-              <Button
-                label="Thêm tài nguyên"
-                onClick={() => {
-                  navigate("/postAsset");
-                }}
-              ></Button>
+          isCreator ? (
+            <Card>
+              <div className="add-asset-card cursor-pointer flex flex-column justify-content-center align-items-center">
+                <i className="pi pi-plus-circle icon m-3" />
+                <Button
+                  label="Thêm tài nguyên"
+                  onClick={() => {
+                    navigate("/artwork/post");
+                  }}
+                ></Button>
+              </div>
             </Card>
           ) : (
             <div> Tác giả chưa có tài nguyên nào </div>
           )
         ) : (
-          assets.map((asset) => (
-            console.log("asset", asset),
-            <div className="gallery__item col col-6" key={asset.id}>
-              <AssetsCard
-                key={asset.id}
-                id={asset.id}
-                thumbnail={asset.thumbnail}
-                isCreator={asset.isCreator}
-                itemsList={asset.itemsList}
-              />
-            </div>
-          ))
+          assets.map(
+            (asset) => (
+              (
+                <div className="gallery__item col col-12" key={asset.id}>
+                  <AssetsCard
+                    key={asset.id}
+                    id={asset.id}
+                    thumbnail={asset.thumbnail}
+                    isCreator={asset.isCreator}
+                    itemsList={asset.itemsList}
+                  />
+                </div>
+              )
+            )
+          )
         )}
       </div>
-    </>
+    </div>
   );
 };
 
