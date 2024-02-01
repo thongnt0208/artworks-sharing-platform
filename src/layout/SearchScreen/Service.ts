debc0733c94b2e43e17a6be4e53db6db58363bcd
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-02-01 11:26:55 +0700

    Complete ITG Search Screen

--- src/layout/SearchScreen/Service.ts
+++ src/layout/SearchScreen/Service.ts
@@ -1,4 +1,30 @@
-export async function searchService() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  return response.json();
+import axios from "axios";
+
+export async function searchAll(value: string) {
+  const url = "https://es.huynhvanphu.id.vn/artworks/_search";
+  const username = "elastic";
+  const password = "Matkhausieumanh123";
+
+  const body = {
+    query: {
+      query_string: {
+        query: value,
+        default_field: "*",
+      },
+    },
+  };
+
+  try {
+    const response = await axios.post(url, body, {
+      auth: { username, password },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Response:", response.data);
+    return response;
+  } catch (error: any) {
+    console.error(`Error making search ${value}: `, error);
+    return error;
+  }
 }
