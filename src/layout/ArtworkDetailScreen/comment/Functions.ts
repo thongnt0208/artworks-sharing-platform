e5d0d7094c8f23742171e9c7bdcbc59cb75477be
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-02-18 12:45:01 +0700

    Update fetchComments but waiting for BE response format

--- src/layout/ArtworkDetailScreen/comment/Functions.ts
+++ src/layout/ArtworkDetailScreen/comment/Functions.ts
@@ -1,6 +1,31 @@
 import { addCommentToArtwork } from "../Service";
 import { CommentType } from "../content/ArtworkDetailType";
 
+/**
+ * This function to add a new comment to the artwork
+ *
+ * @description This function will add a new comment to the artwork, then update the list of comments and notify the parent component to reload the comments
+ * @param props - The props of the parents component, which contains the current user data
+ * @param commentValue - The content of the comment
+ * @param setCommentValue - The function to set the content of the comment
+ * @param commentsList - The state that contains list of comments
+ * @param setCommentsList - The function to set the list of comments state
+ * @param setLoading  - The function to set the loading state
+ * @param toast - The ref of the toast component to make notification
+ * @returns void
+ * @example
+ * addComment(
+ *  props,
+ * commentValue,
+ * setCommentValue,
+ * commentsList,
+ * setCommentsList,
+ * setLoading,
+ * toast
+ * );
+ * @version 1.0.1
+ * @author ThongNT
+ */
 export default function addComment(
   props: any,
   commentValue: string,
@@ -22,7 +47,6 @@ export default function addComment(
 
   setCommentValue(""); // Clear input after adding comment
   // Change in UI
-  props?.setIsCommentChanged(true);
   setCommentsList([...commentsList, newComment]);
 
   // Change in Server
@@ -31,6 +55,9 @@ export default function addComment(
 
     // API call
     addCommentToArtwork(props?.artworkId, commentValue)
+      .then(() => {
+        // props?.setIsCommentChanged(true); //notify to reload comments
+      })
       .catch((error) => {
         console.log("Error adding comment:", error);
 
