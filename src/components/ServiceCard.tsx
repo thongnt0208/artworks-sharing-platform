377721ab6648f069afb6ba082a1ae21b41579932
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-02-27 15:47:36 +0700

    Anh - Get and Display Services Data & Delete a Service

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Button } from "primereact/button";
+import { useOutletContext } from "react-router-dom";
 
 import "./ServiceCard.scss";
 const background = require("../assets/defaultImage/default-card-blur-image.png");
 
-type Props = {
+export type ServiceProps = {
   id: string;
   serviceName: string;
   description: string;
-  deliveryTime: number;
+  deliveryTime: string;
   numberOfConcept: number;
   numberOfRevision: number;
   startingPrice: number;
   coverLocation: string;
-}
-const ServiceCard: React.FC<Props> = ({ ...props }: Props) => {
+  editHandler?: () => void;
+  hireHandler?: () => void;
+};
+const ServiceCard: React.FC<ServiceProps> = ({ ...props }: ServiceProps) => {
+  const [isCreator] = useOutletContext() as [boolean];
+
   return (
     <div
       className="service-card-container "
@@ -27,7 +32,9 @@ const ServiceCard: React.FC<Props> = ({ ...props }: Props) => {
           <h2 className="service-name mt-0 mb-0 ml-3 font-bold">
             {props.serviceName}
           </h2>
-          <p className="starting-price mt-0 mb-0 ml-3">Từ {props.startingPrice} Xu</p>
+          <p className="starting-price mt-0 mb-0 ml-3">
+            Từ {props.startingPrice} Xu
+          </p>
           <p className="cover-location mt-0 mb-0 ml-3">{props.coverLocation}</p>
         </div>
         <div className="service-card-details-bottom mt-2 ml-3">
@@ -39,13 +46,24 @@ const ServiceCard: React.FC<Props> = ({ ...props }: Props) => {
             {props.numberOfRevision} lần chỉnh sửa
           </p>
         </div>
-        <div className="hire-button-container w-full flex justify-content-center mt-3 mb-3 ">
-          <Button
-            label="Thuê"
-            rounded
-            className="hire-button border-none pl-5 pr-5"
-          />
-        </div>
+        {isCreator ? (
+          <div className="hire-button-container w-full flex justify-content-center mt-3 mb-3 ">
+            <Button
+              label="Chỉnh sửa"
+              rounded
+              className="hire-button border-none pl-5 pr-5"
+              onClick={props.editHandler}
+            />
+          </div>
+        ) : (
+          <div className="hire-button-container w-full flex justify-content-center mt-3 mb-3 ">
+            <Button
+              label="Thuê"
+              rounded
+              className="hire-button border-none pl-5 pr-5"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
