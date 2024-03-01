2ea98f511ccca42e59dcdb24f3fda2650fb3a4f8
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-03-01 17:47:11 +0700

    Anh - Add Formik

---
+++ src/layout/ProfileScreen/ServicesView/ServiceInformationSection/FormikData.ts
@@ -0,0 +1,11 @@
+import * as Yup from "yup";
+
+export const validationSchema = Yup.object().shape({
+    serviceName: Yup.string().required('Tên dịch vụ là bắt buộc'),
+    description: Yup.string().required('Mô tả dịch vụ là bắt buộc'),
+    deliveryTime: Yup.string().required('Thời gian hoàn thành là bắt buộc'),
+    numberOfConcept: Yup.number().required('Số lượng thể loại là bắt buộc'),
+    numberOfRevision: Yup.number().required('Số lần chỉnh sửa là bắt buộc'),
+    startingPrice: Yup.number().required('Giá khởi điểm là bắt buộc'),
+  });
+  
