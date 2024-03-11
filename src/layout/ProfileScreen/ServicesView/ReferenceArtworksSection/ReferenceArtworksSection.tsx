c2a4e62099fd8dfbe076e744963daa6b1066e50d
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2024-03-11 14:41:26 +0700

    Anh - (ITG) Add new Service

--- src/layout/ProfileScreen/ServicesView/ReferenceArtworksSection/ReferenceArtworksSection.tsx
+++ src/layout/ProfileScreen/ServicesView/ReferenceArtworksSection/ReferenceArtworksSection.tsx
@@ -1,49 +1,124 @@
-import React from "react";
-import { Card } from "primereact/card";
+import React, { useEffect, useRef, useState } from "react";
 import { Checkbox } from "primereact/checkbox";
-import { Carousel, CarouselPageChangeEvent } from "primereact/carousel";
-import { ArtworkProps } from "../../../../components/ArtworkCard";
+import { ScrollPanel } from "primereact/scrollpanel";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+
+import ArtworkCard, { ArtworkProps } from "../../../../components/ArtworkCard";
+import "./ReferenceArtworksSection.scss";
 
 interface ReferenceArtworksSectionProps {
   artworks: ArtworkProps[];
-  onPageChange: (event: CarouselPageChangeEvent) => void;
+  selectArtworks: (artworkIds: string[]) => void;
+  setShow: (close: boolean) => void,
+  loadData: () => void;
 }
 
 const ReferenceArtworksSection: React.FC<ReferenceArtworksSectionProps> = ({
   artworks,
-  onPageChange,
+  selectArtworks,
+  setShow,
+  loadData,
 }) => {
-  // const [checkedArtworksId, setCheckedArtworksId] = useState<string[]>([]);
+  const toast = useRef<Toast | null>(null);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastArtworkRef = useRef<HTMLDivElement | null>(null);
+  const [selectedArtworkIds, setSelectedArtworkIds] = useState<Record<string, boolean>>(
+    JSON.parse(localStorage.getItem("selectedArtworkIds") || "{}")
+  );
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadData();
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (lastArtworkRef.current && observer.current) {
+      observer.current.observe(lastArtworkRef.current);
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [loadData]);
+
+  const handleCheckboxChange = (artworkId: string) => {
+    setSelectedArtworkIds((prevSelected) => ({
+      ...prevSelected,
+      [artworkId]: !prevSelected[artworkId],
+    }));
+  };
 
   const artworkItems = (artwork: ArtworkProps) => {
     return (
-      <Card
-        title={artwork.title}
-        subTitle={artwork.createdBy}
-        style={{ width: "450px", height: "553px" }}
-      >
-        <img
-          src={artwork.thumbnail}
-          alt={artwork.title}
-          style={{ width: "100%", height: "15rem" }}
+      <div className="artwork-item" key={artwork.id}>
+        <ArtworkCard onSelection={true} {...artwork} />
+        <Checkbox
+          className="checkbox"
+          value={artwork.id}
+          checked={selectedArtworkIds[artwork.id] || false}
+          onChange={() => handleCheckboxChange(artwork.id)}
         />
-        <p>{artwork.title}</p>
-        <Checkbox inputId="cb1" value="Artwork" checked={false} />
-      </Card>
+      </div>
     );
   };
 
+  const handleSaveClick = () => {
+    const selectedIds = Object.keys(selectedArtworkIds).filter((id) => selectedArtworkIds[id]);
+    if (selectedIds.length === 0) {
+      showError();
+    } else {
+      selectArtworks(selectedIds);
+      setShow(false)
+    }
+  };
+
+  const showError = () => {
+    toast.current?.show({
+      severity: "error",
+      summary: "Lỗi",
+      detail: "Bạn phải chọn ít nhất 1 tác phẩm",
+      life: 3000,
+    });
+  };
+
+  // Save selectedArtworkIds to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("selectedArtworkIds", JSON.stringify(selectedArtworkIds));
+  }, [selectedArtworkIds]);
+
   return (
     <div className="reference-artworks-section w-full">
-      <h2>Reference Artworks</h2>
-      <Carousel
-        className="w-full"
-        value={artworks}
-        numVisible={3}
-        numScroll={3}
-        itemTemplate={artworkItems}
-        onPageChange={onPageChange}
-      />
+      <Toast ref={toast} />
+      <div className="header">
+        <div className="title">
+          <h2>Chọn các tác phẩm</h2>
+          <h4>
+            Thêm tác phẩm để cho khách hàng tiềm năng biết bạn sẵn sàng làm gì và giúp họ dễ
+            dàng thuê bạn.
+          </h4>
+        </div>
+        <div className="action">
+          <Button label="Lưu" className="p-button" onClick={handleSaveClick} />
+        </div>
+      </div>
+
+      <ScrollPanel style={{ height: "650px" }} className="artwork-scroll-panel">
+        <div className="artwork-list">
+          {artworks.map((artwork) => {
+            return artworkItems(artwork);
+          })}
+        </div>
+        <div ref={lastArtworkRef}>
+          {/* This is an invisible marker to observe */}
+        </div>
+      </ScrollPanel>
     </div>
   );
 };
--- src/layout/ProfileScreen/ServicesView/ServiceInformationSection/FormikData.ts
+++ src/layout/ProfileScreen/ServicesView/ServiceInformationSection/FormikData.ts
@@ -1,11 +1,11 @@
 import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-    serviceName: Yup.string().required('Tên dịch vụ là bắt buộc'),
-    description: Yup.string().required('Mô tả dịch vụ là bắt buộc'),
-    deliveryTime: Yup.string().required('Thời gian hoàn thành là bắt buộc'),
-    numberOfConcept: Yup.number().required('Số lượng thể loại là bắt buộc'),
-    numberOfRevision: Yup.number().required('Số lần chỉnh sửa là bắt buộc'),
-    startingPrice: Yup.number().required('Giá khởi điểm là bắt buộc'),
-  });
-  
+  serviceName: Yup.string().required("Tên dịch vụ là bắt buộc"),
+  description: Yup.string().required("Mô tả dịch vụ là bắt buộc"),
+  deliveryTime: Yup.string().required("Thời gian hoàn thành là bắt buộc"),
+  numberOfConcept: Yup.number().required("Số lượng thể loại là bắt buộc"),
+  numberOfRevision: Yup.number().required("Số lần chỉnh sửa là bắt buộc"),
+  startingPrice: Yup.number().required("Giá khởi điểm là bắt buộc"),
+  // referenceArtworks: Yup.array().of(Yup.string()).required("Tác phẩm tham khảo là bắt buộc"),
+});
