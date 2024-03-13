5810b37e6800093478a67ecec8dccdcbae6d378a
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2024-03-13 15:25:11 +0700

    Anh_(ITG)Deposit-money

--- src/layout/ProfileScreen/WalletView/DepositCoin/DepositCoin.tsx
+++ src/layout/ProfileScreen/WalletView/DepositCoin/DepositCoin.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Dialog } from "primereact/dialog";
 import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
 import { Button } from "primereact/button";
 import { InputNumber } from "primereact/inputnumber";
-import "./DepositCoin.scss";
+import { Toast } from 'primereact/toast';
 
+import { DepositCoins } from "../WalletService";
+import "./DepositCoin.scss";
 interface Method {
   name: string;
   code: string;
@@ -14,7 +16,16 @@ const DepositCoin: React.FC<{ isVisible: boolean; onHide: () => void }> = ({
   isVisible,
   onHide,
 }) => {
+  let currentUrl = window.location.href;
+  const toast = useRef<Toast>(null);
   const [selectedMethod, setSelectedMethod] = useState<Method | null>(null);
+  const [amount, setAmount] = useState(0);
+  const [amountValidationMessage, setAmountValidationMessage] = useState<
+    string | null
+  >(null);
+  const [methodValidationMessage, setMethodAmountValidationMessage] = useState<
+    string | null
+  >(null);
   const methodList: Method[] = [
     { name: "ZaloPay", code: "ZALOPAY" },
     { name: "Momo", code: "MOMO" },
@@ -22,12 +33,39 @@ const DepositCoin: React.FC<{ isVisible: boolean; onHide: () => void }> = ({
     { name: "Tài khoản ngân hàng", code: "BANK" },
   ];
 
-  const handleDeposit = () => {
-    // Integrate
+  const handleDeposit = async () => {
+    if (amount < 1000 && selectedMethod === null) {
+      setAmountValidationMessage("Số Xu cần nạp ít nhất là 1000 Xu");
+      setMethodAmountValidationMessage("Hãy chọn phương thức thanh toán");
+      return;
+    }
+    if (selectedMethod?.code === "ZALOPAY") {
+      console.log("ZALOPAY");
+      try {
+        
+        const response = await DepositCoins(amount, currentUrl);
+        if (response.returnCode === 1) {
+          window.location.href = response.orderUrl;
+        } else {
+          showError();
+        }
+      } catch (error) {
+        showError();
+      }
+    }
+    setMethodAmountValidationMessage(null);
+    setSelectedMethod(null);
+    setAmount(0);
+    onHide();
   };
+
+  const showError = () => {
+    toast.current?.show({severity:'error', summary: 'Nạp Xu thất bại', detail:'Xảy ra lỗi trong quá trình nạp Xu', life: 3000});
+  }
 
   return (
     <>
+      <Toast ref={toast} />
       <Dialog
         closable={false}
         visible={isVisible}
@@ -41,16 +79,39 @@ const DepositCoin: React.FC<{ isVisible: boolean; onHide: () => void }> = ({
             <label className="method-label">Phương thức thanh toán</label>
             <Dropdown
               value={selectedMethod}
-              onChange={(e: DropdownChangeEvent) => setSelectedMethod(e.value)}
+              onChange={(e: DropdownChangeEvent) => {
+                setSelectedMethod(e.value);
+                setMethodAmountValidationMessage(null);
+              }}
               options={methodList}
               optionLabel="name"
               placeholder="Hãy chọn một phương thức"
               className="w-fit md:w-14rem"
             />
+            {methodValidationMessage && (
+              <span className="validation-message text-red-500">
+                {methodValidationMessage}
+              </span>
+            )}
           </div>
           <div className="amount-input">
             <label className="amount-label">Số Xu cần nạp</label>
-            <InputNumber className="w-full md:w-14rem" />
+            <InputNumber
+              className="w-full md:w-14rem"
+              onValueChange={(e) => {
+                setAmount(e.value || 0);
+                setAmountValidationMessage(
+                  e.value && e.value < 1000
+                    ? "Số Xu cần nạp ít nhất là 1000 Xu"
+                    : null
+                );
+              }}
+            />
+            {amountValidationMessage && (
+              <span className="validation-message text-red-500">
+                {amountValidationMessage}
+              </span>
+            )}
           </div>
           <div className="action-button">
             <Button
