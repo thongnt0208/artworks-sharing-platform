0e0078571e69ff6b2db1e9f1eb5c1c91bc701ece
『Zenith』 <phuhuynh923@gmail.com>
2024-03-19 12:33:27 +0700

    [Phu][Fix Login UI]

--- src/layout/RegisterScreen/RegisterScreen.tsx
+++ src/layout/RegisterScreen/RegisterScreen.tsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./RegisterScreen.scss";
 import logo from "../../assets/logo/logo_notext.svg";
 import logotext from "../../assets/logo/logo.png";
-import {  InputText,  Button,  Toast,  Divider,  Image,  Card,  useFormik,  Password,  Calendar} from "../index";
+import { InputText, Button, Toast, Divider, Image, Card, useFormik, Password, Calendar } from "../index";
 import { getAuthInfo } from "../../util/AuthUtil";
 import { initalValues, yupObject } from "./FormikData";
 import { register } from "../../auth/AuthService";
@@ -90,89 +90,98 @@ const RegisterScreen = () => {
   return (
     <>
       <Toast ref={toast} />
-      <div className="register-container flex w-100">
+      <div className="register-container">
         <div className="background-overlay"></div>
-        <div className="logo-container p-4 hidden lg:block">
-          <Image alt="logo" src={logotext} height="100" />
-        </div>
-        <Card className="register-card">
-          <div className="header-container pb-4">
-            <div className="logo flex justify-content-start lg:hidden">
-              <Image alt="logo" src={logo} height="40" />
-              <h3 className="m-0">Artworkia</h3>
-            </div>
-            <h1>Đăng ký</h1>
-            <span>Người dùng cũ?</span> <Link to={"/login"}>Đăng nhập</Link>
-          </div>
-          <div className="normal-register">
-            {/* Form begin */}
-            <form onSubmit={formik.handleSubmit}>
-              <div className="username-container">
-                {renderError("username")}
-                <InputText
-                  id="username"
-                  {...formik.getFieldProps("username")}
-                  placeholder="Tên đăng nhập"
-                  maxLength={20}
-                />
-              </div>
-              <div className="password-container">
-                {renderError("password")}
-                <Password
-                  id="password"
-                  {...formik.getFieldProps("password")}
-                  footer={pwFooter}
-                  placeholder="Mật khẩu"
-                  toggleMask
-                />
-              </div>
-              <div className="confirm-password-container">
-                {renderError("confirmPassword")}
-                <Password
-                  id="confirmPassword"
-                  {...formik.getFieldProps("confirmPassword")}
-                  placeholder="Nhập lại mật khẩu"
-                  feedback={false}
-                />
-              </div>
-              <Divider />
-              {/* Email InputText */}
-              <div className="email-container">
-                {renderError("email")}
-                <InputText id="email" {...formik.getFieldProps("email")} placeholder="Email" />
-              </div>
-              {/* Fullname InputText */}
-              <div className="fullname-container">
-                {renderError("fullname")}
-                <InputText
-                  id="fullname"
-                  {...formik.getFieldProps("fullname")}
-                  placeholder="Tên đầy đủ"
-                />
-              </div>
-              {/* birthdate InputText */}
-              <div className="birthdate-container">
-                {renderError("birthdate")}
-                <Calendar
-                  id="birthdate"
-                  name="birthdate"
-                  value={formik.values.birthdate}
-                  onChange={(e) => formik.setFieldValue("birthdate", e.value)}
-                  onBlur={formik.handleBlur}
-                  placeholder="Ngày sinh"
-                  dateFormat="dd/mm/yy"
-                  maxDate={today}
-                />
-              </div>
-              <Button label="Tiếp tục" type="submit" className="mt-3" disabled={!formik.isValid} />
-            </form>
-            {/* Form end */}
-          </div>
 
-          <Divider />
-          <div className="3party-register">
-            <div className="google-register"></div>
-            <div className="facebook-register"></div>
+        <Card className="bg-white max-h-full overflow-y-auto">
+          <div className="flex justify-content-center align-content-center">
+            <div className="logo-container flex justify-content-center align-items-center">
+              <div className="p-8 hidden lg:block">
+                <Image alt="logo" src={logotext} height="100" />
+              </div>
+            </div>
+            <div className="hidden lg:block">
+              <Divider layout="vertical" />
+            </div>
+            <div className="register-card">
+              <div className="header-container pb-4">
+                <div className="logo flex justify-content-start align-items-center lg:hidden">
+                  <Image alt="logo" src={logo} height="40" />
+                  <h3 className="m-0">Artworkia</h3>
+                </div>
+                <h1>Đăng ký</h1>
+                <span>Người dùng cũ?</span> <Link to={"/login"}>Đăng nhập</Link>
+              </div>
+              <div className="normal-register">
+                {/* Form begin */}
+                <form onSubmit={formik.handleSubmit}>
+                  <div className="username-container">
+                    {renderError("username")}
+                    <InputText
+                      id="username"
+                      {...formik.getFieldProps("username")}
+                      placeholder="Tên đăng nhập"
+                      maxLength={20}
+                    />
+                  </div>
+                  <div className="password-container">
+                    {renderError("password")}
+                    <Password
+                      id="password"
+                      {...formik.getFieldProps("password")}
+                      footer={pwFooter}
+                      placeholder="Mật khẩu"
+                      toggleMask
+                    />
+                  </div>
+                  <div className="confirm-password-container">
+                    {renderError("confirmPassword")}
+                    <Password
+                      id="confirmPassword"
+                      {...formik.getFieldProps("confirmPassword")}
+                      placeholder="Nhập lại mật khẩu"
+                      feedback={false}
+                    />
+                  </div>
+                  {/* Email InputText */}
+                  <div className="email-container">
+                    {renderError("email")}
+                    <InputText id="email" {...formik.getFieldProps("email")} placeholder="Email" />
+                  </div>
+                  {/* Fullname InputText */}
+                  <div className="fullname-container">
+                    {renderError("fullname")}
+                    <InputText
+                      id="fullname"
+                      {...formik.getFieldProps("fullname")}
+                      placeholder="Tên đầy đủ"
+                    />
+                  </div>
+                  {/* birthdate InputText */}
+                  <div className="birthdate-container">
+                    {renderError("birthdate")}
+                    <Calendar
+                      id="birthdate"
+                      name="birthdate"
+                      value={formik.values.birthdate}
+                      onChange={(e) => formik.setFieldValue("birthdate", e.value)}
+                      onBlur={formik.handleBlur}
+                      placeholder="Ngày sinh"
+                      dateFormat="dd/mm/yy"
+                      maxDate={today}
+                    />
+                  </div>
+                  <Button label="Tiếp tục" type="submit" className="mt-3" disabled={!formik.isValid} />
+                </form>
+                {/* Form end */}
+              </div>
+
+              <Divider />
+              <div className="3party-register">
+                <div className="google-register"></div>
+                <div className="facebook-register"></div>
+              </div>
+            </div>
           </div>
         </Card>
       </div>
