9f2ce3f646ff4188d83b0def698471afa01f5c0e
『Zenith』 <phuhuynh923@gmail.com>
2024-03-23 16:35:29 +0700

    [Phu][Edit Footer]

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,21 +13,25 @@ const Footer: React.FC = () => {
   const currentUrl = location.pathname + location.search + location.hash;
 
   const items = [
-    <DefaultButton text="Artworkia" icon="" onClick={() => {}} key="logo"/>,
-    <DefaultButton text="Điều khoản" icon="" onClick={() => {}} key="dieu-khoan" />,
-    <DefaultButton text="Quyền riêng tư" icon="" onClick={() => {}} key="quyen-rieng-tu"/>,
-    <DefaultButton text="Trợ giúp" icon="" onClick={() => {}} key="tro-giup"/>,
+    <DefaultButton text="Artworkia" icon="" onClick={() => { }} key="logo" />,
+    <DefaultButton text="Điều khoản" icon="" onClick={() => { }} key="dieu-khoan" />,
+    <DefaultButton text="Quyền riêng tư" icon="" onClick={() => { }} key="quyen-rieng-tu" />,
+    <DefaultButton text="Trợ giúp" icon="" onClick={() => { }} key="tro-giup" />,
   ];
 
-  const endItems = [[<Image src={logo} alt="Logo" />]];
+  const items1 = [
+    { label: 'Artworkia', icon: '' },
+    { label: 'Điều khoản', icon: '' },
+    { label: 'Quyền riêng tư', icon: '' },
+    { label: 'Trợ giúp', icon: '' },
+  ];
+  const endItems = [[<Image src={logo} alt="Logo" height="28" />]];
 
   return (
     <footer
-      className={`footer ${
-        hideFooterRoutes.some((route) => currentUrl.includes(`/${route}`)) ? "hidden" : ""
-      }`}
+      className={`footer ${hideFooterRoutes.some((route) => currentUrl.includes(`/${route}`)) ? "hidden" : "h-3rem"}`}
     >
-      <Menubar className="menubar" start={items} end={endItems} />
+      <Menubar className="menubar fixed w-full bottom-0" model={items1} end={endItems} />
     </footer>
   );
 };
