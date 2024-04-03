396d1e2ef19484ce18e3929d4c8a98165e76c395
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-03 13:38:42 +0700

    Thong - Update UI of Chat components, Add refresh btn

--- src/components/ChatboxItem.tsx
+++ src/components/ChatboxItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./styles/ChatboxItem.scss";
 import { Avatar } from "primereact/avatar";
 import { ChatboxItemType } from "../layout/ChatScreen/ChatRelatedTypes";
@@ -11,15 +11,14 @@ type ChatboxItemTemplateProps = {
 
 const ChatboxItemTemplate: React.FC<ChatboxItemTemplateProps> = (props) => {
   const { item, selectingChatbox } = props;
-  const { id, avatar, text, author, time, isSeen } = item;
-  const [isSeenLocal, setIsSeenLocal] = useState<boolean>(isSeen);
+  const { id, avatar, text, author, time } = item;
   const navigate = useNavigate();
 
   useEffect(() => {
     const textElements = document.querySelectorAll(".text-limit");
     textElements.forEach((element) => {
       let text = element.textContent?.trim() || "";
-      if (text.length > 30) {
+      if (text.length > 20) {
         text = text.slice(0, 30) + "...";
         element.textContent = text;
       }
@@ -33,12 +32,8 @@ const ChatboxItemTemplate: React.FC<ChatboxItemTemplateProps> = (props) => {
       : `chatbox-item chatbox-item-selecting ${flexAddition}`;
 
   const itemClickHandler = () => {
-    // Call a function that calls an API to setIsSeen to false
-    // setIsSeenLocal(true);
-    setIsSeenLocal(true);
     navigate(`/chat/${id}`);
     console.log("ClickedId: ", id);
-    
   };
 
   return (
@@ -50,19 +45,10 @@ const ChatboxItemTemplate: React.FC<ChatboxItemTemplateProps> = (props) => {
         <div className="chatbox-item-header">
           <div className="chatbox-item-author text-cus-normal-bold">{author?.fullname}</div>
         </div>
-        <div
-          className={
-            !isSeenLocal
-              ? "chatbox-item-text text-cus-normal-bold text-limit"
-              : "chatbox-item-text text-cus-normal text-limit"
-          }
-        >
-          {text}
-        </div>
+        <div className="chatbox-item-text text-cus-normal text-limit">{text}</div>
       </div>
       <div className="message-status-content">
         <div className="chatbox-item-time text-cus-small">{time}</div>
-        {!isSeenLocal && <div className="chatbox-item-seen" />}
       </div>
     </div>
   );
