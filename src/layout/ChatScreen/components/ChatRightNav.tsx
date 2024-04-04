557d764d6238f0c2511f6fccb0a0097cdf36b7e4
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-04 16:37:29 +0700

    Thong - Complete Proposal Complete payment

--- src/layout/ChatScreen/components/ChatRightNav.tsx
+++ src/layout/ChatScreen/components/ChatRightNav.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
-import { MilestoneItemType, ProposalAssetItemType, ProposalType } from "../ChatRelatedTypes";
-import "./ChatRightNav.scss";
 import { Avatar } from "primereact/avatar";
-import { Button } from "primereact/button";
-import { formatTime } from "../../../util/TimeHandle";
-import { translateProposalStatus } from "../../../util/Enums";
-import MilestoneView from "./MilestoneViewSection/MilestoneView";
+import "./ChatRightNav.scss";
+import MilestoneView from "./MilestoneView/MilestoneView";
+import ProposalAssetsView from "./ProposalAssetsView/ProposalAssetsView";
+import ProposalsListView from "./Proposal/ProposalsListView";
+import {
+  MilestoneItemType,
+  ProposalAssetItemType,
+  ProposalStateToolsType,
+} from "../ChatRelatedTypes";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   userInfo: {
@@ -17,25 +21,23 @@ type Props = {
     fullname: string;
     avatar: string;
   };
-  proposalsList: ProposalType[];
-  selectingProposal: ProposalType;
-  setSelectingProposal: (proposal: ProposalType) => void;
+  proposalStateTools: ProposalStateToolsType;
   currentMilestone: MilestoneItemType[];
-  currentAsset: ProposalAssetItemType[];
+  currentAssets: ProposalAssetItemType[];
   getMilestoneCallback: (id: string) => void;
   getAssetsCallback: (id: string) => void;
 };
 
 export default function ChatRightNav({
   userInfo,
-  proposalsList,
-  selectingProposal,
-  setSelectingProposal,
+  proposalStateTools,
   currentMilestone,
-  currentAsset,
+  currentAssets,
   getMilestoneCallback,
   getAssetsCallback,
 }: Props) {
+  const navigate = useNavigate();
+  const { proposalsList, selectingProposal, setSelectingProposal } = proposalStateTools;
   useEffect(() => {
     if (selectingProposal?.id) {
       getMilestoneCallback(selectingProposal.id);
@@ -45,77 +47,22 @@ export default function ChatRightNav({
 
   return (
     <div className="chat-right-nav">
-      <div className="user-info-container">
+      <div
+        className="user-info-container"
+        onClick={() => navigate(`/account/${selectingProposal.createdBy}`)}
+      >
         <Avatar size="xlarge" image={userInfo.avatar} shape="circle" />
         <p className="text-cus-h1-bold">{userInfo.fullname}</p>
         <p className="text-cus-normal">{userInfo.username}</p>
         <p className="text-cus-normal">{userInfo.bio}</p>
       </div>
-      {/* Proposal area start */}
-      <div className="proposals-list-container">
-        <div className="proposals-list-header">
-          <p className="text-cus-h2-bold">Các thỏa thuận</p>
-        </div>
-        <div className="proposals-list-content">
-          {proposalsList?.map((proposal) => (
-            <div
-              key={proposal.id}
-              className={"proposal-item" + (proposal.id === selectingProposal?.id ? " active" : "")}
-              onClick={() => setSelectingProposal(proposal)}
-            >
-              <div className="first-collumn-frame">
-                <p className="text-cus-normal-bold">{proposal.projectTitle}</p>
-                <p className="text-cus-normal">
-                  Đến hạn: {formatTime(proposal.targetDelivery, "dd/MM/yyyy")}
-                </p>
-              </div>
-              <div className="second-collumn-frame">
-                <p className="text-cus-normal highlight-btn-100">
-                  {translateProposalStatus(proposal.status)}
-                </p>
-                <p className="text-cus-normal highlight-btn-200">{proposal.totalPrice}Xu</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      {/* Proposal area end */}
-      {/* Milestone area start */}
-      <div className="milestone-view-container">
-        <div className="milestone-view-header pb-4">
-          <p className="text-cus-h2-bold">Theo dõi quá trình</p>
-        </div>
-        <div className="milestone-view-content">
-          <MilestoneView data={currentMilestone} />
-        </div>
-      </div>
-      {/* Milestone area end */}
-      {/* Asset area start */}
-      <div className="proposal-assets-container">
-        <div className="proposal-assets-header">
-          <p className="text-cus-h2-bold">Tài nguyên liên quan</p>
-        </div>
-        <div className="proposal-assets-content">
-          {currentAsset?.map((asset) => (
-            <div className="proposal-asset-item">
-              <Button
-                label={asset.id}
-                icon="pi pi-download"
-                iconPos="right"
-                onClick={() => {
-                  //download file from asset?.url
-                  window.open(asset?.url, "_blank");
-                }}
-                className="first-collumn-frame"
-              />
-              <p className="text-cus-normal second-collumn-frame">
-                {formatTime(asset.createdOn, "dd/MM/yyyy HH:mm")}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-      {/* Asset area end */}
+      <ProposalsListView
+        data={proposalsList}
+        selectingProposal={selectingProposal}
+        setSelectingProposal={setSelectingProposal}
+      />
+      <MilestoneView data={currentMilestone} />
+      <ProposalAssetsView data={currentAssets} proposalStateTools={proposalStateTools} />
     </div>
   );
 }
---
+++ src/layout/ChatScreen/components/ProposalAssetsView/ProposalAssetsView.tsx
@@ -0,0 +1,66 @@
+import { Button } from "primereact/button";
+import { formatTime } from "../../../../util/TimeHandle";
+import { ProposalAssetItemType, ProposalStateToolsType } from "../../ChatRelatedTypes";
+import "./ProposalAssetsView.scss";
+import { getAuthInfo } from "../../../../util/AuthUtil";
+
+type Props = { data: ProposalAssetItemType[]; proposalStateTools: ProposalStateToolsType };
+
+export default function ProposalAssetsView({ data, proposalStateTools }: Props) {
+  const { selectingProposal, handleCompletePayment } = proposalStateTools;
+  const authenticationInfo = getAuthInfo();
+  let currentUserId = authenticationInfo?.id ? authenticationInfo?.id : "unknown";
+
+  return (
+    <div className="proposal-assets-container">
+      <div className="proposal-assets-header">
+        <p className="text-cus-h2-bold">Tài nguyên liên quan</p>
+      </div>
+      <div className="proposal-assets-content">
+        {data?.map((asset) => (
+          <div className="proposal-asset-item">
+            <Button
+              label="Tệp đính kèm"
+              icon="pi pi-download"
+              iconPos="right"
+              disabled={
+                // check if asset is final && proposal status is not CompletePayment && current user is not the creator
+                asset.type === "Final" &&
+                selectingProposal.status !== "CompletePayment" &&
+                selectingProposal.createdBy !== currentUserId
+              }
+              onClick={() => {
+                //download file from asset?.url
+                window.open(asset?.url, "_blank");
+              }}
+              className="w-full mr-3"
+            />
+            <p className="text-cus-normal second-collumn-frame">
+              {formatTime(asset.createdOn, "dd/MM/yyyy HH:mm")}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="complete-payment-container">
+        {
+          // check if there is a final asset and proposal status is not CompletePayment -> Start to complete payment
+          data.some((asset) => asset.type === "Final") &&
+            selectingProposal.status !== "CompletePayment" &&
+            selectingProposal.createdBy !== currentUserId && (
+              <Button
+                label="Trả tiền đủ"
+                icon="pi pi-check"
+                className="w-full"
+                onClick={() => {
+                  //complete payment
+                  // alert("complete payment");
+                  handleCompletePayment && handleCompletePayment(selectingProposal.id);
+                }}
+              />
+            )
+        }
+      </div>
+    </div>
+  );
+}
