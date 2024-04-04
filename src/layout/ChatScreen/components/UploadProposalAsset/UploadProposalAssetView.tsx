712c5ec6a3c4868e46f470e5f01b4c7814500a8e
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-04 23:19:36 +0700

    Thong - Fix Upload Asset Bugs

---
+++ src/layout/ChatScreen/components/UploadProposalAsset/UploadProposalAssetView.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { getAuthInfo } from "../../../../util/AuthUtil";
+import { FileUpload } from "primereact/fileupload";
+import { ProposalType } from "../../ChatRelatedTypes";
+import { maxSizeImagesUpload } from "../../../../const/bizConstants";
+import { RadioButton } from "primereact/radiobutton";
+import { Button } from "primereact/button";
+
+type Props = {
+  selectingProposal: ProposalType;
+  uploadAssetCallback: (proposalId: string, type: number, file: File) => void;
+};
+
+export default function UploadProposalAssetView({ selectingProposal, uploadAssetCallback }: Props) {
+  const authenticationInfo = getAuthInfo();
+  let currentUserId = authenticationInfo?.id ? authenticationInfo?.id : "unknown";
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [selectedType, setSelectedType] = useState<number | null>(null);
+
+  const typeOptions = [
+    { label: "Concept", value: 0 },
+    { label: "Final", value: 1 },
+    { label: "Revision", value: 2 },
+  ];
+  const handleUploadAsset = () => {
+    console.log("handleUploadAsset", selectingProposal?.id, selectedType, uploadedFile);
+
+    if (selectedType !== null && uploadedFile) {
+      uploadAssetCallback && uploadAssetCallback(selectingProposal?.id, selectedType, uploadedFile);
+      setUploadedFile(null);
+      setSelectedType(null);
+    }
+  };
+  return (
+    <div>
+      <div className="btns-container">
+        Thỏa thuận đã được chấp nhận.
+        {selectingProposal?.createdBy === currentUserId && (
+          <div className="upload-proposal-assets-container">
+            <FileUpload
+              mode="basic"
+              chooseLabel="Chọn tệp"
+              accept="*"
+              auto
+              maxFileSize={maxSizeImagesUpload}
+              onSelect={(event) => setUploadedFile(event.files[0])}
+            />
+            <div className="radio-buttons flex gap-2 justify-content-center">
+              {typeOptions.map((option) => (
+                <div
+                  key={option.label}
+                  className="radio-button-container flex gap-1 align-items-center"
+                >
+                  <RadioButton
+                    value={option.value}
+                    checked={selectedType === option.value}
+                    onChange={(e) => setSelectedType(e.value)}
+                  />
+                  <label>{option.label}</label>
+                </div>
+              ))}
+            </div>
+            <Button
+              disabled={selectedType === null && uploadedFile === null}
+              onClick={handleUploadAsset}
+              rounded
+            >
+              Gửi tệp
+            </Button>
+          </div>
+        )}
+        {selectingProposal?.createdBy !== currentUserId && (
+          <p>Từ giờ, nhà sáng tạo sẽ làm việc và gửi bản thảo đến bạn!</p>
+        )}
+      </div>
+    </div>
+  );
+}
