b6cc78e2de577889cb722bb9c2351677b775492a
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-08 22:43:08 +0700

    Anh - ITG Asset View

--- src/layout/ProfileScreen/AssetsView/AssetsService.tsx
+++ src/layout/ProfileScreen/AssetsView/AssetsService.tsx
@@ -1,19 +1,95 @@
-import axios from "axios";
+import { axiosPrivate } from "../../../hooks/useAxios";
+import { AssetsProps } from "../../../components/AssetsCard";
+import { BoughtAssetsProps } from "./BoughtAssetsSection/BoughtAssets";
 
-export async function GetAssetsData(accountId: string) {
-    try {
-      const response = await axios.get(`http://127.0.0.1:1880/account/${accountId}/assets`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.status !== 200) {
-        console.log("Error fetching artworks data");
-        return [];
-      }
-      return response.data;
-    } catch (error) {
-      console.log("Error fetching artworks data:", error);
+export async function GetAssetsData(
+  accountId: string,
+  pageNumber: number,
+  pageSize: number
+): Promise<AssetsProps[]> {
+  try {
+    const response = await axiosPrivate.get(`/accounts/${accountId}/assets`, {
+      params: {
+        pageNumber,
+        pageSize,
+      },
+    });
+    if (response.status !== 200) {
       return [];
+    } else {
+      let assetsData: AssetsProps[] = [];
+      if (Array.isArray(response.data.items)) {
+        assetsData = response.data.items.map((asset: any) => ({
+          id: asset.id,
+          title: asset.title,
+          description: asset.description,
+          thumbnail: asset.thumbnail,
+          viewCount: asset.viewCount,
+          likeCount: asset.likeCount,
+          commentCount: asset.commentCount,
+          privacy: asset.privacy,
+          state: asset.state,
+          createdBy: asset.createdBy,
+          createdOn: asset.createdOn,
+          itemsList: asset.assets.map((asset: any) => ({
+            id: asset.id,
+            name: asset.assetTitle,
+            price: asset.price,
+            description: asset.assetTitle,
+            extension: asset.extension,
+            size: asset.size,
+            thumbnail: asset.thumbnail,
+            lastModificatedOn: asset.lastModificatedOn,
+            assetType: asset.assetType,
+          })),
+        }));
+      }
+      return assetsData;
     }
+  } catch (error) {
+    console.log(error);
+    return [];
   }
+}
+
+export async function GetBoughtAssetsData(
+  accountId: string,
+  pageNumber: number,
+  pageSize: number
+): Promise<BoughtAssetsProps[]> {
+  try {
+    const response = await axiosPrivate.get(
+      `/accounts/${accountId}/assets-bought`,
+      {
+        params: {
+          pageNumber,
+          pageSize,
+        },
+      }
+    );
+    if (response.status !== 200) {
+      return [];
+    } else {
+      let assetsData: BoughtAssetsProps[] = [];
+
+      if (Array.isArray(response.data.items)) {
+        assetsData = response.data.items.map((asset: any) => ({
+          id: asset.id,
+          artworkId: asset.artworkId,
+          order: asset.order,
+          assetTitle: asset.assetTitle,
+          description: asset.description,
+          assetName: asset.assetName,
+          price: asset.price,
+          isBought: asset.isBought,
+          fileMetaData: asset.fileMetaData,
+          lastModificatedOn: asset.lastModificatedOn,
+        }));
+      }
+      return assetsData;
+    }
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
