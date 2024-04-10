68e3007cc0e0a3219a0cfbbb9bd7ec17083c8dcd
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-10 12:54:16 +0700

    Thong - Edit comment feature

--- src/layout/ArtworkDetailScreen/comment/CommentItem.tsx
+++ src/layout/ArtworkDetailScreen/comment/CommentItem.tsx
@@ -1,11 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { getAuthInfo } from "../../../util/AuthUtil";
 import { Button } from "primereact/button";
 import { Image } from "primereact/image";
-import { removeCommentFromArtwork } from "../Service";
+import { editCommentOnArtwork, removeCommentFromArtwork } from "../Service";
 import { CommentType } from "../ArtworkDetailType";
 import { Toast } from "primereact/toast";
 import { ConfirmPopup, confirmPopup } from "primereact/confirmpopup";
+import { formatTime } from "../../../util/TimeHandle";
+import { InputText } from "primereact/inputtext";
+import { maxCommentCharacter } from "../../../const/bizConstants";
 
 interface CommentItemProps {
   index: number;
@@ -15,20 +18,24 @@ interface CommentItemProps {
     fullname?: string;
   };
   comment: CommentType;
+  editCommentCallback?: (cmtId: string) => void;
 }
 
 const CommentItem: React.FC<CommentItemProps> = ({
   index,
   profileData,
   comment,
+  editCommentCallback,
 }) => {
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [commentValue, setCommentValue] = useState<string>("");
   const blankPic = require("../../../assets/defaultImage/blank-100.png");
   const currentUserId = getAuthInfo()?.id || "";
 
-  // const handleEditComment = () => {
-  //   console.log("Edit comment");
-  // };
+  const handleEditComment = () => {
+    setIsEdit(!isEdit);
+  };
 
   const handleDeleteComment = () => {
     setIsLoading(true);
@@ -73,6 +80,36 @@ const CommentItem: React.FC<CommentItemProps> = ({
     });
   };
 
+  let handleInputChange = (e: any) => {
+    if (e.target.value.length <= maxCommentCharacter) {
+      setCommentValue(e.target.value);
+    }
+  };
+
+  let handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      editCommentOnArtwork(comment.id, commentValue)
+        .then(() => {
+          toast?.current?.show({
+            severity: "success",
+            summary: "Chỉnh sửa bình luận thành công",
+          });
+          setIsEdit(false);
+        })
+        .catch((error) => {
+          console.log("Error editing comment:", error);
+          toast?.current?.show({
+            severity: "error",
+            summary: "Chỉnh sửa thất bại",
+            detail: `${error}`,
+            life: 3000,
+          });
+          setIsEdit(false);
+        });
+    }
+  };
+
   return (
     <>
       <Toast ref={toast} />
@@ -81,9 +118,24 @@ const CommentItem: React.FC<CommentItemProps> = ({
         <div className="grid flex">
           <div className="flex col col-9 justify-content-start">
             <Image src={profileData?.avatar || blankPic} alt={profileData?.fullname} />
+
             <div className="flex flex-column gap-1 justify-content-center">
-              <span className="text-cus-normal-bold">{profileData?.fullname}</span>
-              <span className="content text-cus-normal">{comment.content}</span>
+              <span className="text-cus-normal-bold">
+                {profileData?.fullname}{" "}
+                <span className="text-cus-normal" style={{ color: "gray" }}>
+                  lúc {formatTime(comment.createdOn, "dd tháng MM/yyyy HH:mm")}
+                </span>
+              </span>
+              {!isEdit && <span className="content text-cus-normal">{comment.content}</span>}
+              {isEdit && (
+                <InputText
+                  className="p-inputtext-sm"
+                  tooltip="Enter để lưu"
+                  defaultValue={comment.content}
+                  onChange={(e: any) => handleInputChange(e)}
+                  onKeyDown={(e: any) => handleKeyDown(e)}
+                />
+              )}
             </div>
           </div>
 
@@ -91,7 +143,7 @@ const CommentItem: React.FC<CommentItemProps> = ({
             {/* Edit and Delete icon button */}
             {currentUserId === profileData.id && (
               <div className="flex gap-1 justify-content-center">
-                {/* <Button icon={"pi pi-pencil"} rounded onClick={handleEditComment} /> */}
+                <Button icon={"pi pi-pencil"} rounded onClick={handleEditComment} />
                 <Button icon={"pi pi-trash"} rounded onClick={confirmDelete} loading={isLoading} />
               </div>
             )}
