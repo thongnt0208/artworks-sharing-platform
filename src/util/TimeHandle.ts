a4ffc2ebf5206794837cd356016c914babdf1c9a
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-11 01:09:03 +0700

    Anh - Change time format to UTC+7

--- src/util/TimeHandle.ts
+++ src/util/TimeHandle.ts
@@ -19,7 +19,7 @@ export const formatTime = (
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-    const hours = String(date.getHours()).padStart(2, '0');
+    const hours = String(date.getHours() + 7).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
   
