a4e8fafb8d235a85cc9f66692486fca32a9557ea
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-11 16:10:09 +0700

    Anh - Enhance get reference artwork in service view

--- src/layout/ProfileScreen/ArtworksView/ArtworksService.tsx
+++ src/layout/ProfileScreen/ArtworksView/ArtworksService.tsx
@@ -2,34 +2,40 @@ import { axiosPrivate } from "../../../hooks/useAxios";
 import { ArtworkProps } from "../../../components/ArtworkCard";
 
 /**
- * 
+ *
  * Retrieves artworks data from the server.
- * 
+ *
  * @param pageSize - The number of artworks to retrieve per page.
  * @param pageNumber - The page number of the artworks to retrieve.
  * @param accountId - The ID of the account to retrieve artworks for.
  * @returns A promise that resolves to the artworks data.
- * @author AnhDH 
+ * @author AnhDH
  * @version 1.2.0
  */
-export async function GetArtworksData(pageSize: number, pageNumber: number, accountId: string, state?: number) {
+export async function GetArtworksData(
+  pageSize: number,
+  pageNumber: number,
+  accountId: string,
+  state?: number, 
+  privacy?: number
+) {
   try {
-    const response = await axiosPrivate.get(
-      `/accounts/${accountId}/artworks`,
-      {
-        params: {
-          pageNumber: pageNumber,
-          pageSize: pageSize,
-          state: state,
-        }
-      }
-    );
+    const response = await axiosPrivate.get(`/accounts/${accountId}/artworks`, {
+      params: {
+        pageNumber: pageNumber,
+        pageSize: pageSize,
+        sortColumn: "create", 
+        sortOrder: "desc",
+        state: state,
+        privacy: privacy,
+      },
+    });
     if (response.status !== 200) {
       return [];
     } else {
       let artworksData: ArtworkProps[] = [];
       if (Array.isArray(response.data.items)) {
-          artworksData = response.data.items.map((artwork: any) => ({
+        artworksData = response.data.items.map((artwork: any) => ({
           id: artwork.id,
           title: artwork.title,
           thumbnail: artwork.thumbnail,
@@ -48,19 +54,17 @@ export async function GetArtworksData(pageSize: number, pageNumber: number, acco
 }
 
 /**
- * 
+ *
  * Deletes an artwork from the server.
- * 
+ *
  * @param artworkId - The ID of the artwork to delete.
  * @returns A boolean indicating whether the artwork was successfully deleted.
- * @author AnhDH 
+ * @author AnhDH
  * @version 1.0.0
  */
 export async function DeleteArtworkData(artworkId: string) {
   try {
-    await axiosPrivate.delete(
-      `/artworks/${artworkId}`
-    );
+    await axiosPrivate.delete(`/artworks/${artworkId}`);
     return true;
   } catch (error) {
     return false;
