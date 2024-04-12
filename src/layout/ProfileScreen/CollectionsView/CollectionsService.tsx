6bbdb013e92856d2fe3aa21585564999383cf078
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-13 00:30:48 +0700

    Anh - Fix displaying numOfAw in Collection Card

--- src/layout/ProfileScreen/CollectionsView/CollectionsService.tsx
+++ src/layout/ProfileScreen/CollectionsView/CollectionsService.tsx
@@ -1,17 +1,23 @@
 import axios from "../../../hooks/useAxios";
+import { CollectionProps } from "../../CollectionDetailScreen/CollectionDetailScreen";
 
-export async function GetCollectionsData(account_id: string) {
+export async function GetCollectionsData(
+  account_id: string
+): Promise<CollectionProps[]> {
   try {
-    const response = await axios.get(
-      `/account/${account_id}/collections`
-    );
-    if (response.status !== 200) {
-      console.log("Error fetching artworks data");
-      return [];
-    }
-    return response.data;
+    const response = await axios.get(`/account/${account_id}/collections`);
+    return response.data.map((item: any) => {
+      return {
+        id: item.id,
+        creatorFullName: item.createdBy.fullname,
+        collectionName: item.collectionName,
+        privacy: item.privacy,
+        numberOfArtworks: item.items,
+        accountAvatar: item.createdBy.accountAvatar,
+        thumbnail: item.thumbnail,
+      };
+    });
   } catch (error) {
-    console.log("Error fetching artworks data:", error);
-    return [];
+    throw new Error("Lấy dữ liệu thất bại.");
   }
 }
