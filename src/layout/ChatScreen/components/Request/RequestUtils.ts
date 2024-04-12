b3ac82f29e1b4412109da6786707ae6d5c08df49
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-12 10:04:53 +0700

    Thong - Fix some bugs in ChatScreen

--- src/layout/ChatScreen/components/Request/RequestUtils.ts
+++ src/layout/ChatScreen/components/Request/RequestUtils.ts
@@ -4,37 +4,35 @@ import { GetRequestsByChatboxId, UpdateRequestStatus } from "../../services/Prop
 import { CatchAPICallingError } from "../../..";
 
 export const GetAllRequests = (
-    selectingChatbox: ChatboxItemType,
-    setRequestsList: (requestsList: RequestItemType[]) => void,
-    navigate: ReturnType<typeof useNavigate>
-  ) => {
-    selectingChatbox?.id &&
-      GetRequestsByChatboxId(selectingChatbox?.id)
-        .then((res) => {
-          setRequestsList(res);
-        })
-        .catch((error) => {
-          setRequestsList([]);
-          CatchAPICallingError(error, navigate);
-        });
-  };
-  
-  export const acceptRequest = (
-    id: string,
-    GetAllRequests: Function,
-    navigate: ReturnType<typeof useNavigate>
-  ) => {
-    UpdateRequestStatus(id, 1)
-      .then(() => GetAllRequests())
-      .catch((error) => CatchAPICallingError(error, navigate));
-  };
-  
-  export const denyRequest = (
-    id: string,
-    GetAllRequests: Function,
-    navigate: ReturnType<typeof useNavigate>
-  ) => {
-    UpdateRequestStatus(id, 2)
-      .then(() => GetAllRequests())
-      .catch((error) => CatchAPICallingError(error, navigate));
-  };
+  selectingChatbox: ChatboxItemType,
+  setRequestsList: (requestsList: RequestItemType[]) => void,
+  navigate: ReturnType<typeof useNavigate>
+) => {
+  selectingChatbox?.id &&
+    GetRequestsByChatboxId(selectingChatbox?.id)
+      .then((res) => setRequestsList(res))
+      .catch((error) => {
+        setRequestsList([]);
+        CatchAPICallingError(error, navigate);
+      });
+};
+
+export const acceptRequest = (
+  id: string,
+  GetAllRequests: Function,
+  navigate: ReturnType<typeof useNavigate>
+) => {
+  UpdateRequestStatus(id, 1)
+    .then(() => GetAllRequests())
+    .catch((error) => CatchAPICallingError(error, navigate));
+};
+
+export const denyRequest = (
+  id: string,
+  GetAllRequests: Function,
+  navigate: ReturnType<typeof useNavigate>
+) => {
+  UpdateRequestStatus(id, 2)
+    .then(() => GetAllRequests())
+    .catch((error) => CatchAPICallingError(error, navigate));
+};
