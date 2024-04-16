72a32362f3597d14ed1d6d2e7f86de144334421f
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-16 21:34:55 +0700

    Thong - Add recommend aws in explore screen, edit TagCarousel UI

--- src/const/uiConstants.ts
+++ src/const/uiConstants.ts
@@ -14,3 +14,10 @@ export const hideHeaderRoutes = [
   "reset-password",
   "change-password",
 ];
+
+export const cookieNames = {
+  accessToken: "access_token",
+  refreshToken: "refresh_token",
+  userInfo: "user_info",
+  interactedArtworks: "interacted_artworks_ids",
+}
