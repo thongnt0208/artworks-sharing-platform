50a45f882f2617e43ea4ceb040f1e2323e99e7f3
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-21 00:09:46 +0700

    Thong - Add policy page, helmet meta data; Fix warnings

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -3,14 +3,14 @@ import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const logo = require("../assets/logo/logo.png");
-export default function Forbidden401() {
+export default function PolicyPage() {
   return (
     <>
       <Helmet>
-        <title>401 Không được truy cập</title>
+        <title>Chính sách nền tảng</title>
         <meta
           name="description"
-          content="Rất tiếc, bạn không có quyền truy cập trang này. Vui lòng liên hệ với quản trị viên để được hỗ trợ."
+          content="Chính sách nền tảng của chúng tôi giúp bạn hiểu rõ hơn về cách chúng tôi thu thập, sử dụng dữ liệu và các cam kết mà bạn phải chấp nhận khi sử dụng hệ thống của chúng tôi."
         />
       </Helmet>
       <div className="err-page-container flex align-items-center justify-content-center min-h-screen overflow-hidden">
@@ -28,11 +28,29 @@ export default function Forbidden401() {
               className="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center"
               style={{ borderRadius: "53px" }}
             >
-              <span className="text-blue-500 font-bold text-3xl">401</span>
-              <h1 className="text-900 font-bold text-5xl mb-2">Không được truy cập</h1>
+              <span className="text-blue-500 font-bold text-3xl">Chi tiết</span>
+              <h1 className="text-900 font-bold text-5xl mb-2">Chính sách nền tảng</h1>
               <div className="text-600 mb-5">
-                Rất tiếc, bạn không có quyền truy cập trang này. Vui lòng liên hệ với quản trị viên
-                để được hỗ trợ.
+                Chính sách nền tảng của chúng tôi giúp bạn hiểu rõ hơn về cách chúng tôi thu thập,
+                sử dụng dữ liệu và các cam kết mà bạn phải chấp nhận khi sử dụng hệ thống của chúng
+                tôi.
+              </div>
+              <div className="detail-policy-container">
+                <h4 className="text-900 font-bold text-xl mb-2">1. Chính sách bảo mật</h4>
+                <p className="text-800 text-base">
+                  Chính sách bảo mật của chúng tôi giúp bạn hiểu rõ cách chúng tôi thu thập, sử dụng
+                  và bảo vệ thông tin cá nhân của bạn khi bạn sử dụng dịch vụ của chúng tôi.
+                </p>
+                <h4 className="text-900 font-bold text-xl mb-2">2. Chính sách sử dụng</h4>
+                <p className="text-800 text-base">
+                  Chính sách sử dụng của chúng tôi giúp bạn hiểu rõ cách chúng tôi thu thập, sử dụng
+                  và chia sẻ thông tin cá nhân của bạn khi bạn sử dụng dịch vụ của chúng tôi.
+                </p>
+                <h4 className="text-900 font-bold text-xl mb-2">3. Chính sách bảo mật</h4>
+                <p className="text-800 text-base">
+                  Chính sách bảo mật của chúng tôi giúp bạn hiểu rõ cách chúng tôi thu thập, sử dụng
+                  và bảo vệ thông tin cá nhân của bạn khi bạn sử dụng dịch vụ của chúng tôi.
+                </p>
               </div>
               <Link
                 to="/"
@@ -47,7 +65,7 @@ export default function Forbidden401() {
                 <span className="ml-4 flex flex-column">
                   <span className="text-900 lg:text-xl font-medium mb-1">Câu hỏi thường gặp</span>
                   <span className="text-600 lg:text-lg">
-                    Có lẽ bạn đã mắc phải một số lỗi quen thuộc.
+                    Có lẽ bạn sẽ cần biết những điều quen thuộc.
                   </span>
                 </span>
               </Link>
