743b07cf3e102bceee40025b9a59de948b8ef8ba
Thong Nguyen Trung <trungthongnguyen2002@gmail.com>
2024-04-26 17:57:15 +0700

    Thong - Update platformFee

--- src/const/bizConstants.ts
+++ src/const/bizConstants.ts
@@ -1,20 +1,20 @@
 export const maxNumberOfTags = 10;
 export const maxNumberOfCategories = 3;
-export const platformFee = 0.1; //10%
+export const platformFee = 0.05; // 5%
 export const maxCommentCharacter = 500;
 export const maxSizeImagesUpload = 50000000; //50MB
 export const maxSizeAssetsUpload = 50000000; //50MB
 export const minTextLength = 20;
 export const deliveryTime = [
-    "Dưới 1 tuần", 
-    "Trong khoảng 1-2 tuần", 
-    "Trong khoảng 1 tháng", 
-    "Trong khoảng 2 tháng", 
-    "Trong khoảng 3 tháng",
-]
+  "Dưới 1 tuần",
+  "Trong khoảng 1-2 tuần",
+  "Trong khoảng 1 tháng",
+  "Trong khoảng 2 tháng",
+  "Trong khoảng 3 tháng",
+];
 export const sortOptions = [
-    { name: "Đề xuất", code: "recommend" },
-    { name: "Mới nhất", code: "newest" },
-    { name: "Xem nhiều nhất", code: "view" },
-    { name: "Thích nhiều nhất", code: "like" },
-  ];
+  { name: "Đề xuất", code: "recommend" },
+  { name: "Mới nhất", code: "newest" },
+  { name: "Xem nhiều nhất", code: "view" },
+  { name: "Thích nhiều nhất", code: "like" },
+];
