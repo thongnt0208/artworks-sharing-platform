0c318859183f17d2642b58bfea59d7d7a55645c9
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-28 12:51:59 +0700

    Anh - itg edit profile

--- src/layout/ProfileSettingsScreen/ProfileSettingsService.tsx
+++ src/layout/ProfileSettingsScreen/ProfileSettingsService.tsx
@@ -1,26 +1,62 @@
-import axios from "axios";
-import { getAuthInfo } from "../../util/AuthUtil";
-const API_URL = process.env.REACT_APP_REAL_API_BASE_URL;
+import { axiosPrivate } from "../../hooks/useAxios";
 
-const accessToken = getAuthInfo()?.accessToken || "";
-const refreshToken = getAuthInfo()?.refreshToken || "";
-
+/**
+ * Retrieves the profile data for a given account ID.
+ * @param accountId - The ID of the account.
+ * @returns A Promise that resolves to the profile data.
+ * @author AnhDH
+ * @version 1.0.0
+ */
 export async function GetProfileData(accountId: string) {
-    try {
-        const response = await axios.get(`${API_URL}/accounts/${accountId}`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${accessToken || refreshToken}`,
-            },
-        });
-        if (response.status !== 200) {
-            console.log("Error fetching profile data");
-            return [];
-        }
-        return response.data;
+  try {
+    const response = await axiosPrivate.get(`/accounts/${accountId}`);
+    if (response.status !== 200) {
+      return [];
     }
-    catch (error) {
-        console.log("Error fetching profile data:", error);
-        return [];
+    return response.data;
+  } catch (error) {
+    return [];
+  }
+}
+
+export async function UpdateProfileData(accountId: string, data: any): Promise<any> {
+  try {
+    const response = await axiosPrivate.put(`/accounts/${accountId}`, data);
+    if (response.status !== 200) {
+      return [];
     }
+    return response.data;
+  } catch (error) {
+    throw new Error("Cập nhật thông tin thất bại. Vui lòng thử lại sau.");
+  }
+}
+
+export async function UpdateProfileAvatar(accountId: string, avatar: File): Promise<any> {
+  try {
+    const formData = new FormData();
+    formData.append("avatar", avatar);
+    const response = await axiosPrivate.put(`/accounts/${accountId}/avatar`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (response.status !== 200) {
+      return [];
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error("Cập nhật ảnh đại diện thất bại. Vui lòng thử lại sau.");
+  }
+}
+
+export async function UpdatePassword(accountId: string, data: any): Promise<any> {
+  try {
+    const response = await axiosPrivate.put(`/accounts/${accountId}/password`, data);
+    if (response.status !== 200) {
+      return [];
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error("Cập nhật thông tin thất bại. Vui lòng thử lại sau.");
+  }
 }
